46c48ba8718bd1e7b6a8f46ce973d9bc2ccebae5
xx <784457618@qq.com>
2021-06-23 23:57:40 +0800

    print config process

--- gorm-plugin/db/g_db.go
+++ gorm-plugin/db/g_db.go
@@ -2,13 +2,14 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"github.com/laxiaohong/lollipop/gorm-plugin/db/config"
 	"github.com/laxiaohong/lollipop/gorm-plugin/log"
+	"github.com/sirupsen/logrus"
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	gormLogger "gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 	"time"
 )
 
@@ -17,13 +18,18 @@ type DB struct {
 }
 
 func NewDB(cfg *config.MySQLConfig, logger *zap.Logger) (*DB, func()) {
-	fmt.Printf("NewDB config:%#v", cfg.String())
-	db, err := gorm.Open(mysql.Open(cfg.GetDsn()), &gorm.Config{
-		Logger: log.NewLogger(logger, gormLogger.Config{
-			SlowThreshold: time.Duration(cfg.LogConfig.GetSlowLogSecond()) * time.Second,
-			LogLevel:      gormLogger.Info,
-		}),
-	})
+	const _prefixLog = "NewDB: "
+	logrus.Infof("NewDB config:%#v", cfg.String())
+	namingStrategy := schema.NamingStrategy{SingularTable: true, NoLowerCase: false}
+	logrus.Info(namingStrategy)
+	db, err := gorm.Open(mysql.Open(cfg.GetDsn()),
+		&gorm.Config{
+			Logger: log.NewLogger(logger, gormLogger.Config{
+				SlowThreshold: time.Duration(cfg.LogConfig.GetSlowLogSecond()) * time.Second,
+				LogLevel:      gormLogger.Info,
+			}),
+			NamingStrategy: namingStrategy,
+		})
 	if err != nil {
 		panic(err)
 	}
@@ -33,36 +39,43 @@ func NewDB(cfg *config.MySQLConfig, logger *zap.Logger) (*DB, func()) {
 		panic(err)
 	}
 	if cfg.GetConnConfig().MaxOpenConn != nil {
+		logrus.Info(_prefixLog, "maxIdleConn", cfg.GetConnConfig().GetMaxIdleConn())
 		_db.SetMaxOpenConns(int(cfg.GetConnConfig().GetMaxOpenConn()))
 	} else {
+		logrus.Warn(_prefixLog, "maxIdleConn_default", 100)
 		_db.SetMaxOpenConns(100)
 	}
 
 	if cfg.GetConnConfig().ConnMaxLifeSecond != nil {
+		logrus.Info(_prefixLog, "maxIdleSecond", time.Duration(cfg.GetConnConfig().GetConnMaxIdleSecond()*int64(time.Second)).String())
 		_db.SetConnMaxLifetime(time.Duration(cfg.GetConnConfig().GetConnMaxIdleSecond()) * time.Second)
 	} else {
+		logrus.Warn(_prefixLog, "maxIdleSecond", time.Duration(3600*time.Second).String())
 		_db.SetConnMaxLifetime(3600 * time.Second)
 	}
 
 	if cfg.GetConnConfig().ConnMaxIdleSecond != nil {
+		logrus.Info(_prefixLog, "maxIdleConn", cfg.GetConnConfig().GetMaxIdleConn())
 		_db.SetMaxIdleConns(int(cfg.GetConnConfig().GetMaxIdleConn()))
 	} else {
+		logrus.Warn(_prefixLog, "maxIdleConn", 100)
 		_db.SetMaxIdleConns(100)
 	}
 
 	if cfg.GetConnConfig().ConnMaxIdleSecond != nil {
+		logrus.Info(_prefixLog, "connMaxIdleSecond", time.Duration(cfg.GetConnConfig().GetConnMaxIdleSecond()*int64(time.Second)).String())
 		_db.SetConnMaxIdleTime(time.Duration(cfg.GetConnConfig().GetConnMaxIdleSecond()) * time.Second)
 	} else {
+		logrus.Warn(_prefixLog, "connMaxIdleSecond", (3600 * time.Second).String())
 		_db.SetConnMaxIdleTime(3600 * time.Second)
 	}
 
 	return &DB{db: db}, func() {
 		err := _db.Close()
-		fmt.Println("mysql close , err info ", err)
+		logrus.Warn("close db ", "mysql close , err info ", err)
 	}
 }
 
 func (c *DB) WithContext(ctx context.Context) *gorm.DB {
-
 	return c.db.WithContext(ctx)
 }

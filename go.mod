c8d3016546a29c15022cf81466f847a7265f54ef
xx <784457618@qq.com>
2021-07-14 11:39:02 +0800

    otel 更新包

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/arl/statsviz v0.4.0
 	github.com/go-kratos/consul v0.1.1
 	github.com/go-kratos/etcd v0.1.1
-	github.com/go-kratos/kratos/v2 v2.0.0-rc6
+	github.com/go-kratos/kratos/v2 v2.0.0
 	github.com/google/wire v0.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/hashicorp/consul/api v1.9.0
@@ -14,14 +14,13 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cast v1.3.1
 	go.etcd.io/etcd/client/v3 v3.5.0-rc.1
-	go.opentelemetry.io/otel v0.20.0
-	go.opentelemetry.io/otel/exporters/trace/jaeger v0.20.0
-	go.opentelemetry.io/otel/sdk v0.20.0
-	go.opentelemetry.io/otel/trace v0.20.0
+	go.opentelemetry.io/otel v1.0.0-RC1
+	go.opentelemetry.io/otel/exporters/jaeger v1.0.0-RC1
+	go.opentelemetry.io/otel/sdk v1.0.0-RC1
+	go.opentelemetry.io/otel/trace v1.0.0-RC1
 	go.uber.org/zap v1.17.0
-	google.golang.org/genproto v0.0.0-20210624195500-8bfb893ecb84 // indirect
-	google.golang.org/grpc v1.38.0
-	google.golang.org/protobuf v1.27.0
+	google.golang.org/grpc v1.39.0
+	google.golang.org/protobuf v1.27.1
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gorm.io/driver/mysql v1.1.1
 	gorm.io/gorm v1.21.11
--- kratos-plugin/traceing/provider/otel/provider.go
+++ kratos-plugin/traceing/provider/otel/provider.go
@@ -2,10 +2,11 @@ package otel
 
 import (
 	"github.com/sirupsen/logrus"
-	"go.opentelemetry.io/otel/exporters/trace/jaeger"
+
+	"go.opentelemetry.io/otel/exporters/jaeger"
 	"go.opentelemetry.io/otel/sdk/resource"
 	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/semconv"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 type Config struct {
@@ -18,17 +19,18 @@ type Config struct {
 func NewTracerProvider(cfg Config) (*tracesdk.TracerProvider, error) {
 	logrus.Infof("cfg %#v", cfg)
 	// Create the Jaeger exporter
-	exp, err := jaeger.NewRawExporter(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(cfg.Endpoint)))
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(cfg.Endpoint)))
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
 
 	tp := tracesdk.NewTracerProvider(
 		tracesdk.WithSampler(tracesdk.TraceIDRatioBased(cfg.Fraction)),
 		// Always be sure to batch in production.
 		tracesdk.WithBatcher(exp),
+
 		// Record information about this application in an Resource.
-		tracesdk.WithResource(resource.NewWithAttributes(
+		tracesdk.WithResource(resource.NewSchemaless(
 			semconv.ServiceNameKey.String(cfg.ServiceName),
 			semconv.ServiceVersionKey.String(cfg.Version),
 		)),
